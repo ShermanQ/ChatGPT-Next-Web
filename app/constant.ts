71b170ee18f2b00c3964f1940c1ca0a02d8d0fab
Toogle <25702506+ShermanQ@users.noreply.github.com>
2023-08-06 02:03:32 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -85,20 +85,8 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
-    name: "gpt-4-0613",
-    available: false,
-  },
-  {
     name: "gpt-4-32k",
     available: true,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: false,
-  },
-  {
-    name: "gpt-4-32k-0613",
-    available: false,
   },
   {
     name: "gpt-3.5-turbo",
@@ -109,20 +97,8 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
-    name: "gpt-3.5-turbo-0613",
-    available: false,
-  },
-  {
     name: "gpt-3.5-turbo-16k",
     available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
-    available: false,
-  },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
-    available: false,
   },
   {
     name: "llama-2-70b-chat",
